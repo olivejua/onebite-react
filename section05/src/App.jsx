fc7895607b24abd4af133c84a09d10283a7440c8
olivejua <45840556+olivejua@users.noreply.github.com>
2025-09-09 23:55:50 +0900

    section05 react props 적용

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,8 +2,17 @@ import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Button from './components/Button'
 
 function App() {
+    const buttonProps = {
+        text: "메일",
+        color: "red",
+        a: 1,
+        b: 2,
+        c: 3,
+    }
+
   return (
     <>
         {/*
@@ -11,9 +20,12 @@ function App() {
         반대로 Header에게 App은 부모 컴포넌트가 됨.
         APP 은 모든 컴포넌트의 뿌리역할이라고 해서 루트 컴포넌트라고 함.
         */}
-        <Header />
-        <Main />
-        <Footer />
+
+        <Button {...buttonProps} />
+        <Button text={"카페"} />
+        <Button text={"블로그"} >
+            <Header />
+        </Button>
     </>
   )
 }
