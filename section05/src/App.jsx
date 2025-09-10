5a0951d04a3a4fe491f77c7445dfca1c34c64584
olivejua <45840556+olivejua@users.noreply.github.com>
2025-09-10 23:31:32 +0900

    section05 state props 적용

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,33 +1,13 @@
 import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
-import Button from './components/Button'
+
+import Register from './components/Register';
 
 function App() {
-    const buttonProps = {
-        text: "메일",
-        color: "red",
-        a: 1,
-        b: 2,
-        c: 3,
-    }
-
-  return (
-    <>
-        {/*
-        App 리턴문 html에 포함되니 Header 컴포넌트는 자식 컴포넌트가 됨.
-        반대로 Header에게 App은 부모 컴포넌트가 됨.
-        APP 은 모든 컴포넌트의 뿌리역할이라고 해서 루트 컴포넌트라고 함.
-        */}
-
-        <Button {...buttonProps} />
-        <Button text={"카페"} />
-        <Button text={"블로그"} >
-            <Header />
-        </Button>
-    </>
-  )
+    return (
+        <>
+            <Register />
+        </>
+    )
 }
 
 export default App
--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,8 +1,18 @@
 const Button = ({children, text, color = "black"}) => {
-    return <button style={{ color: color }}>
-        {text} - {color.toUpperCase()}
-        {children}
-    </button>;
+    // 이벤트 객체
+    const onClickButton = (e) => {
+        console.log(e);
+        console.log(text);
+    }
+
+    return (
+        <button
+            onClick={onClickButton}
+            style={{ color: color }}>
+                {text} - {color.toUpperCase()}
+                {children}
+        </button>
+    );
 }
 
 export default Button;
