8cf71322ec35897815cd743081ce757833d0b86f
olivejua <45840556+olivejua@users.noreply.github.com>
2025-09-14 21:29:45 +0900

    section08 투두리스트 구현

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,16 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
+import {useState} from 'react';
+import Header from './components/Header.jsx'
+import Editor from './components/Editor.jsx'
+import List from "./components/List.jsx";
+import {useFormState} from "react-dom";
+
+const mockData = [
+    {
+        id: 0,
+        isDone: false,
+        content : "React 공부하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content : "빨래하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content : "노래 연습하기",
+        date: new Date().getTime(),
+    },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [todos, setTodos] = useState(mockData);
 
-  return (
-    <>
+    const onCreate = (content) => {
+        const newTodo = {
+            id: 0,
+            isDone: false,
+            content: content,
+            date : new Date().getTime(),
+        };
 
-    </>
-  )
+        setTodos([newTodo, ...todos]);
+    };
+
+    const onUpdate = (targetId) => {
+        setTodos(todos.map(todo =>
+            todo.id === targetId
+                ? { ...todo, isDone: !todo.isDone}
+                : todo
+        ));
+    };
+
+    const onDelete = (targetId) => {
+        setTodos(todos.filter(todo => todo.id !== targetId));
+    }
+
+    return (
+        <div className="App">
+            <Header />
+            <Editor onCreate={onCreate} />
+            <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+        </div>
+    );
 }
 
 export default App
