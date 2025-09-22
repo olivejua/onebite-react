43755f6a564d888f61f0eedaf173835198a23215
olivejua <45840556+olivejua@users.noreply.github.com>
2025-09-22 22:48:34 +0900

    section12 이미지 적용

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,10 @@ import Home from "./pages/Home"
 import Diary from "./pages/Diary"
 import New from "./pages/New"
 import NotFound from "./pages/NotFound"
+import Button from "./components/Button";
+
+import { getEmotionImage } from "./util/get-emotion-image";
+
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -17,6 +21,14 @@ function App() {
 
     return (
         <>
+            <Button />
+            <div>
+                <img src={getEmotionImage(1)} />
+                <img src={getEmotionImage(2)} />
+                <img src={getEmotionImage(3)} />
+                <img src={getEmotionImage(4)} />
+                <img src={getEmotionImage(5)} />
+            </div>
             <div>
                 <Link to="/">Home</Link>
                 <Link to="/new">New</Link>
