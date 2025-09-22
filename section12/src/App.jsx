08013823d5c9c3087910670d94712bd2cf339e16
olivejua <45840556+olivejua@users.noreply.github.com>
2025-09-22 22:00:34 +0900

    section12 라우팅 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,8 +1,24 @@
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home"
+import Diary from "./pages/Diary"
+import New from "./pages/New"
+import NotFound from "./pages/NotFound"
 
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
     return (
-        <></>
+        <>
+            <div>Hello</div>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/new" element={<New />} />
+                <Route exact path="/diary" element={<Diary />} />
+                <Route exact path="*" element={<NotFound />} />
+            </Routes>
+        </>
     );
 }
 
